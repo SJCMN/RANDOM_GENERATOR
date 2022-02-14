228ae05fb524643a943ebb0803c4e331da992e68
SJCMN <sam.clavette@gmail.com>
2022-02-14 15:28:44 -0600

    add event handlers to buttons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'RANDOM_GENERATOR';
+
+  //new property within the class
+
+  // the equivalent code is:
+  // constructor() {
+  //   this.password = '';
+  // }
+
+  includeLetters = false;
+  includeNumbers = false;
+  includeSymbols = false;
+  password ='';
+
+
+  // a method a to trigger a console log every time a user clicks on a button 
+  onButtonClick() {
+    this.password = 'MY PASSWORD!!!';
+    console.log("Include letters:", this.includeLetters);
+    console.log("Include numbers:", this.includeNumbers);
+    console.log("Include symbols:", this.includeSymbols);
+  }
+
+  onChangeUseLetters() {
+    this.includeLetters = !this.includeLetters;
+  }
+
+  onChangeUseNumbers() {
+    this.includeNumbers = !this.includeNumbers;
+  }
+
+  onChangeUseSymbols() {
+    this.includeSymbols = ! this.includeSymbols;
+  }
+  
 }
